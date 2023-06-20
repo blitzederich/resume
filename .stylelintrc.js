f539f0ff11c8e930f4720acc4b0db718f6fed748
Alexander Samorodov <blitzederich@gmail.com>
2023-06-21 01:39:38 +0300

    initial

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+// Copyright 2023 Alexander Samorodov <blitzerich@gmail.com>
+
+module.exports = {
+	'plugins': [
+		'stylelint-order'
+	],
+	'rules': {
+		'order/properties-alphabetical-order': true
+	}
+}
